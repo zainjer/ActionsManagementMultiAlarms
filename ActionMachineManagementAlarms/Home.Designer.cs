53ac2c11276679b30162fd27426f6d45fe21424c
Muhammad Areeb <arainareeb@gmail.com>
2020-08-11 23:08:33 +0500

    Initial Commit

---
+++ ActionMachineManagementAlarms/Home.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ActionsManagementMultiAlarms
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnl_headBar = new System.Windows.Forms.Panel();
+            this.txt_title = new System.Windows.Forms.TextBox();
+            this.pnl_headBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Close.BackColor = System.Drawing.Color.Firebrick;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Microsoft Sans Serif", 3.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Close.ForeColor = System.Drawing.Color.Firebrick;
+            this.btn_Close.Location = new System.Drawing.Point(579, 0);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(20, 20);
+            this.btn_Close.TabIndex = 0;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(557, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 20);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pnl_headBar
+            // 
+            this.pnl_headBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.pnl_headBar.Controls.Add(this.button1);
+            this.pnl_headBar.Controls.Add(this.btn_Close);
+            this.pnl_headBar.Location = new System.Drawing.Point(0, 0);
+            this.pnl_headBar.Margin = new System.Windows.Forms.Padding(0);
+            this.pnl_headBar.Name = "pnl_headBar";
+            this.pnl_headBar.Size = new System.Drawing.Size(600, 20);
+            this.pnl_headBar.TabIndex = 2;
+            // 
+            // txt_title
+            // 
+            this.txt_title.BackColor = System.Drawing.Color.Black;
+            this.txt_title.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txt_title.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_title.ForeColor = System.Drawing.Color.White;
+            this.txt_title.Location = new System.Drawing.Point(137, 23);
+            this.txt_title.Name = "txt_title";
+            this.txt_title.Size = new System.Drawing.Size(326, 25);
+            this.txt_title.TabIndex = 3;
+            this.txt_title.Text = " AMMA - Actions Management Multi Alarms";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.txt_title);
+            this.Controls.Add(this.pnl_headBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Amma";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.pnl_headBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnl_headBar;
+        private System.Windows.Forms.TextBox txt_title;
+    }
+}
+
